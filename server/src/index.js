1cf8669fefbb1ccf2c9131e16d604399d89efb66
raaaimund <raimund.rittnauer@gmail.com>
2022-12-29 20:14:08 +0100

    enable ssl settings using env variables

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,25 @@
+const fs = require('fs');
 const express = require('express')
 const cors = require('cors')
 const http = require("http");
 const {ExpressPeerServer} = require('peer');
 
 const port = process.env.PORT || 9000
+const isProxied = process.env.PROXIED || false
+const isSsl = process.env.SSL || false
+const sslKeyFilePath = process.env.SSL_KEY || undefined
+const sslCertFilePath = process.env.SSL_CERT || undefined
+const sslConfig = isSsl ? {
+    key: fs.readFileSync(sslKeyFilePath),
+    cert: fs.readFileSync(sslCertFilePath)
+} : undefined
 const app = express()
 const server = http.createServer(app);
 const peerServer = ExpressPeerServer(server, {
     path: '/chat',
-    allow_discovery: true
+    allow_discovery: true,
+    proxied: isProxied,
+    ssl: sslConfig
 });
 
 peerServer.on('connection', (client) => {
