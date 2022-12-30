0f85a8df6cdb7a1cfc80dc90bd10dac221fc0bdb
raaaimund <raimund.rittnauer@gmail.com>
2022-12-30 15:38:56 +0100

    renamed refs

--- client/src/components/Connected.tsx
+++ client/src/components/Connected.tsx
@@ -3,26 +3,26 @@ import Button from "./Button";
 import {useEffect, useRef} from "react";
 
 export default function Connected(props: ConnectedProps) {
-    const messageInputRef = useRef({} as HTMLInputElement);
-    const messageContainerRef = useRef({} as HTMLDivElement);
+    const messageInput = useRef({} as HTMLInputElement);
+    const messagesContainer = useRef({} as HTMLDivElement);
 
     useEffect(() => {
-        messageContainerRef.current.scrollTo(0, messageContainerRef.current.scrollHeight)
-    }, [messageContainerRef.current.scrollHeight])
+        messagesContainer.current.scrollTo(0, messagesContainer.current.scrollHeight)
+    }, [messagesContainer.current.scrollHeight])
 
     function handleSendMessage(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault()
-        const text = messageInputRef.current.value
+        const text = messageInput.current.value
         props.handleSendMessage(text)
-        messageInputRef.current.value = ""
-        messageInputRef.current.focus()
+        messageInput.current.value = ""
+        messageInput.current.focus()
     }
 
     return (
         <div className={"flex flex-col justify-end h-full"}>
             <div
                 className={"overflow-y-auto pb-24 scrollbar scrollbar-thin scrollbar-track-pink-100 scrollbar-thumb-pink-500 scroll-smooth"}
-                ref={messageContainerRef}>
+                ref={messagesContainer}>
                 {props.messages.map((message, index) => (
                     <ChatMessage key={index} isFromMe={message.from === "me"} text={message.text}/>
                 ))}
@@ -30,7 +30,7 @@ export default function Connected(props: ConnectedProps) {
             <form onSubmit={handleSendMessage} className={"fixed w-full bottom-0 p-5 border-t-pink-500 border-t bg-white bg-opacity-90"}>
                 <div className={"flex"}>
                     <div className={"grow"}>
-                        <input type={"text"} ref={messageInputRef}
+                        <input type={"text"} ref={messageInput}
                                className={"border border-pink-500 outline-pink-500 p-2 w-full"}/>
                     </div>
                     <div>
