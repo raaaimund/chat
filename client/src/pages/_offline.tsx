1f05cb6ba970bf06b04020aaba89156c154589a3
raaaimund <raimund.rittnauer@gmail.com>
2022-12-30 16:43:04 +0100

    added offline page for pwa

---
+++ client/src/pages/_offline.tsx
@@ -0,0 +1,7 @@
+export default function _offline() {
+    return (
+        <div className={"flex flex-col justify-center items-center h-full"}>
+            <h1 className={"text-pink-500 text-6xl"}>Offline</h1>
+        </div>
+    )
+}
